9c7e827d04dba57dd0dd08358c01f26df05317e1
joaoboger <joaoboger@gmail.com>
2025-07-23 16:24:46 +0200

    dynamical creation of crossword grid

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 // waits the html page to be fully loaded and then
 // executes the arrow function (callback)
+const crosswordConfig = {
+    grid: [
+        [null, 'A', 'A', 'A', null],
+        ['A', 'A', 'A', 'A', 'A'],
+        ['A', 'A', 'A', 'A', 'A'],
+        ['A', 'A', 'A', 'A', 'A'],
+        [null, 'A', 'A', 'A', null]],
+    words : {
+        '1across': {answer: 'BAH', row: 0, col: null},
+        '2across': {answer: 'CACAC', row: 1, col: null},
+        '3across': {answer: 'CACAC', row: 2, col: null},
+        '4across': {answer: 'CACAC', row: 3, col: null},
+        '5across': {answer: 'BAB', row: 4, col: null},
+        '1down': {answer: 'BAB', row: null, col: 1},
+        '2down': {answer: 'CACAC', row: null, col: 2},
+        '3down': {answer: 'CACAC', row: null, col: 3},
+        '4down': {answer: 'CACAC', row: null, col: 0},
+        '5down': {answer: 'BAB', row: null, col: 4},
+    }
+}
+
+function generateGrid(config) {
+    const container = document.querySelector('.cw-grid');
+    container.innerHTML = '';
+
+    config.grid.forEach((row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+            const cellDiv = document.createElement('div');
+
+            if (cell == null) {
+                cellDiv.className = 'cw-cell-null';
+            } else {
+                cellDiv.className = 'cw-cell';
+
+                const input = document.createElement('input');
+                input.className = 'cw-input';
+                input.type = 'text';
+                input.maxLength = 1;
+                input.id = `cell-${rowIndex}-${colIndex}`;
+                
+                cellDiv.appendChild(input);
+            };
+            container.appendChild(cellDiv);
+        });
+    });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("botao-verificar");
-
-    // when clicked, the arrow functions is executed
-    button.addEventListener("click", () => {
-        const value_word = document.getElementById("input-palavra").value.trim();
-        const value_hint = document.getElementById("input-dica").value.trim();
-        const message = document.getElementById("mensagem");
-        
-        if (value_word && value_hint) {
-            message.innerText = "Enviado, obrigado pela dica! Pereta.";
-            message.style.color = "green";
-        }
-
-        else {
-            message.innerText = "Preenche direito, as duas caixas! Pereta."
-            message.style.color = "red";
-        }
-    })
-
-})
+    generateGrid(crosswordConfig);
+});
